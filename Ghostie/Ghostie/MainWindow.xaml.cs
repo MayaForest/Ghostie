5c3078290df88e8e4f2e69abe35d0da36359d0e0
Maya Forest <117468602+MayaForest@users.noreply.github.com>
2024-03-06 02:03:44 -0500

    Uploaded Project

---
+++ Ghostie/Ghostie/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Ghostie
+{
+    public partial class MainWindow : Window
+    {
+        private bool _isStarted = false;
+
+        private MediaPlayer _mediaPlayer = new MediaPlayer();
+
+        private Stopwatch _stopwatch = new Stopwatch();
+
+        private const short _OneSecondInterval = 1000;
+        private System.Timers.Timer _timer;
+
+        private Sprite _ghostie;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _ghostie = new Sprite(GhostSprite, Dispatcher);
+            ResizeGrid();
+        }
+
+        private void ResizeGrid()
+        {
+            MarginRight.Width = MarginLeft.Width = new GridLength(Width / 50);
+            GhostColumn.Width = new GridLength(Width / 2);
+
+            MarginBottom.Height = MarginTop.Height = new GridLength(Height / 50);
+            GhostRow.Height = new GridLength(Height / 2);
+
+            _ghostie.UpdateSpritePosition();
+        }
+
+        private void MainWindowClick(object sender, MouseButtonEventArgs e)
+        {
+            if (!_isStarted)
+            {
+                InitMusic();
+                InitTime();
+                _isStarted = true;
+            }
+        }
+
+        private void InitMusic()
+        {
+            _mediaPlayer.Open(new Uri("../../Assets/BobaBossa.mp3", UriKind.Relative));
+            _mediaPlayer.Play();
+            _mediaPlayer.MediaEnded += MediaEndedEventHandler;
+        }
+
+        private void MediaEndedEventHandler(object sender, EventArgs e)
+        {
+            _mediaPlayer.Position = TimeSpan.Zero;
+            _mediaPlayer.Play();
+        }
+
+        private void InitTime()
+        {
+            _stopwatch.Start();
+            _timer = new System.Timers.Timer(_OneSecondInterval);
+            _timer.Elapsed += TimerElapsedEventHandler;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        private void TimerElapsedEventHandler(object sender, EventArgs e)
+        {
+            this.Dispatcher.BeginInvoke((ThreadStart)delegate {
+                TimeSpan time = TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
+                Counter.Text = GetCounterText(time);
+            });
+        }
+
+        private string GetCounterText(TimeSpan time)
+        {
+            string message = "You've been distracting Ghostie for \n";
+            if (time.Hours > 0)
+            {
+                message += $"{time.ToString(@"hh")} ";
+                if (time.Hours > 1)
+                {
+                    message += "hours ";
+                }
+                else
+                {
+                    message += "hour ";
+                }
+            }
+            if (time.Minutes > 0)
+            {
+                message += $"{time.ToString(@"mm")} ";
+                if (time.Minutes > 1)
+                {
+                    message += "minutes and ";
+                }
+                else
+                {
+                    message += "minute and ";
+                }
+            }
+            message += $"{time.ToString(@"ss")} ";
+            if (time.Seconds > 1)
+            {
+                message += "seconds.";
+            }
+            else
+            {
+                message += "second.";
+            }
+            return message;
+        }
+
+        private void MainWindowResized(object sender, SizeChangedEventArgs e)
+        {
+            ResizeGrid();
+        }
+
+        private void MuteMusicClick(object sender, RoutedEventArgs e)
+        {
+            if (_isStarted)
+                _mediaPlayer.IsMuted = !_mediaPlayer.IsMuted;
+        }
+                
+    }
+}
---
+++ Ghostie/Ghostie/Sprite.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Timers;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace Ghostie
+{
+    internal class Sprite
+    {
+        private Image _spriteSheet;
+
+        private const short _AnimationFps = 1000 / 20;
+        private System.Timers.Timer _timer;
+
+        private const byte _Height = 48;
+        private const byte _Width = 225;
+        private const byte _NbrOfFrames = 9;
+        private const byte _FrameWidth = 225 / _NbrOfFrames;
+
+        private double xOffset = 100;
+        private byte _frame = 0;
+
+        private RectangleGeometry[] _rectangleGeometries;
+
+        private Dispatcher _mainDispatcher;
+
+        public Sprite(Image spriteSheet, Dispatcher dispatcher)
+        {
+            this._spriteSheet = spriteSheet;
+
+            _mainDispatcher = dispatcher;
+
+            _rectangleGeometries = new RectangleGeometry[_NbrOfFrames];
+            for(int frame = 0; frame < _NbrOfFrames; frame++)
+            {
+                _rectangleGeometries[frame] = new RectangleGeometry();
+                _rectangleGeometries[frame].Rect =  new System.Windows.Rect(frame * _FrameWidth, 0, _FrameWidth, _Height);
+            }
+            UpdateRect();
+
+            _timer = new System.Timers.Timer(_AnimationFps);
+            _timer.Elapsed += TimerElapsedEventHandler;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        private void UpdateRect()
+        {
+            _spriteSheet.Clip = _rectangleGeometries[GetFrame()];
+            UpdateSpritePosition();
+        }
+
+        public byte GetFrame()
+        {
+            return _frame;
+        }
+
+        public void UpdateSpritePosition()
+        {
+            xOffset = (_Width / 2) - (_FrameWidth / 2) - (_FrameWidth * _frame);
+            _spriteSheet.RenderTransform = new TranslateTransform(xOffset, 0);
+        }
+
+        private void TimerElapsedEventHandler(object sender, EventArgs e)
+        {
+            _mainDispatcher.BeginInvoke((ThreadStart)delegate {
+                NextFrame();
+                UpdateRect();
+            });
+        }
+
+        public void NextFrame()
+        {
+            _frame++;
+            _frame %= _NbrOfFrames;
+        }
+
+    }
+}
